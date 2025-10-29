63256613d1ad0606d15f5fc034dab82f5158fd79
Rudransh Rachit Bhatt <rudransh.bhatt120960@marwadiuniversity.ac.in>
2025-10-29 19:51:44 +0530

    fix: add missing Dp import in ServicesScreen.kt

--- Reliefnet-android/app/src/main/java/com/sentrive/reliefnet/userInterface/ServicesScreen.kt
+++ Reliefnet-android/app/src/main/java/com/sentrive/reliefnet/userInterface/ServicesScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
